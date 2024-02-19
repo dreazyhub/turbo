61e44bac6d144dd68a9fa56b5632c0a59653341b
= <=>
2024-02-18 16:29:22 -0800

    my commit

--- dispatch/dispatch/settings.py
+++ dispatch/dispatch/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import dj_database_url
+import os
 from pathlib import Path
 
 
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--)go@3+5h5oz2m@x7b*%@8i4jh1&9d8%yfws#zp!=+@@**g4(x'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ") 
 
 
 # Application definition
@@ -83,7 +84,9 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.parse("postgres://rapid_dispatch_user:MecaHz7N818QHtSbPMSzUSznvWH5nGH8@dpg-cn9b8qq1hbls73df8on0-a.oregon-postgres.render.com/rapid_dispatch")
+database_url = os.environ.get("DATABASE_URL")
+
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
